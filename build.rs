16da6c1731eff186330be55bb231d5c582ab5909
yoofie@gmail.com <yoofie@gmail.com>
2023-12-02 19:45:36 -0500

    Updated build.rs to build C/C++ bindings

--- build.rs
+++ build.rs
@@ -7,9 +7,15 @@ fn main() {
 	let crate_dir = env::var("CARGO_MANIFEST_DIR").expect("CARGO_MANIFEST_DIR env var is not defined");
 	let out_dir = PathBuf::from(env::var("CBINDGEN_OUTPUT").expect("CBINDGEN_OUTPUT env var is not defined"));
 
-	let config = cbindgen::Config::from_file("cbindgen.toml").expect("Unable to find cbindgen.toml configuration file");
+	let c_config =
+		cbindgen::Config::from_file("cbindgen.toml").expect("Unable to find cbindgen.toml configuration file");
+	let cpp_config =
+		cbindgen::Config::from_file("cbindgen.toml").expect("Unable to find cppbindgen.toml configuration file");
 
-	cbindgen::generate_with_config(&crate_dir, config)
+	cbindgen::generate_with_config(&crate_dir, c_config)
 		.unwrap()
-		.write_to_file(out_dir.join("pubApi.h"));
+		.write_to_file(out_dir.join("pubApi_c.h"));
+	cbindgen::generate_with_config(&crate_dir, cpp_config)
+		.unwrap()
+		.write_to_file(out_dir.join("pubApi_cpp.h"));
 }
